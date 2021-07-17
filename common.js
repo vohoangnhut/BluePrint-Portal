f912f9b59d4091cece5a4f04083646e9e3406560
vohoangnhut <vohoangnhut.66@gmail.com>
2021-07-17 18:12:57 +0700

    yo

--- common.js
+++ common.js
@@ -634,13 +634,13 @@ function loadListImage() {
 
 
 
-dragElement(document.getElementById("mydiv"));
+dragElement(document.getElementById("drap-div"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.getElementById(elmnt.id + "-header")) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
